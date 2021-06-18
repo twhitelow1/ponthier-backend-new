7fdf7a7c1a5394ac31a0248fcf89d7353f9a83bf
Todd Whitelow <todd@whitelow.tech>
2021-06-18 00:04:44 -0600

    take aways / from url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-    origins 'https://affectionate-nobel-beb889.netlify.app/'
+    origins 'https://affectionate-nobel-beb889.netlify.app'
 
     resource '*',
       headers: :any,
