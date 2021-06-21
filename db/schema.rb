b9987886fd2800b5fbabbdf01cf338150ef72fc4
Todd Whitelow <todd@whitelow.tech>
2021-06-20 22:40:37 -0600

    create user model and add method and validations to the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_060357) do
+ActiveRecord::Schema.define(version: 2021_06_21_043909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 2021_06_14_060357) do
     t.boolean "building_wash"
     t.boolean "flat_surfaces"
     t.boolean "driveway"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
